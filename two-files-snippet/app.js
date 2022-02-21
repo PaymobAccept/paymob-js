f5734c273da4518f3af83294bd7ca95f3d0bb7c4
Muhammed Moussa <mohamedmostafa@paymob.com>
2022-02-21 16:31:52 +0200

    fix: update checkout button id

--- two-files-snippet/app.js
+++ two-files-snippet/app.js
@@ -1,7 +1,7 @@
 fetch("https://next-stg.paymobsolutions.com/marketplace/secret/ibiza/").then(function (response) {
         return response.json();
     }).then(function (json) {
-        Paymob("pkt_ocyiPARECljo1duevlMSKpn3beCz9z5h").checkoutButton(json.client_secret).mount("#pay-button");
+        Paymob("pkt_ocyiPARECljo1duevlMSKpn3beCz9z5h").checkoutButton(json.client_secret).mount("#paymob-checkout");
     }).catch(function (err) {
         console.error(err);
     });
